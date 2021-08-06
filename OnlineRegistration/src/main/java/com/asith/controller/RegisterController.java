272a5b8b44bfe426cfde337e32d40a3d9e31150b
Asitha Navaratne <asithan476@gmail.com>
2021-08-07 01:06:03 +0530

    Justified comments

--- OnlineRegistration/src/main/java/com/asith/controller/RegisterController.java
+++ OnlineRegistration/src/main/java/com/asith/controller/RegisterController.java
@@ -22,7 +22,7 @@ public class RegisterController extends HttpServlet {
 		String lname = request.getParameter("lname");				/// Last name
 		String email = request.getParameter("email");				/// Email
 		String field = request.getParameter("field");				/// Field of interest
-		String pass = request.getParameter("pass");					/// Password
+		String pass = request.getParameter("pass");				/// Password
 		String cpass = request.getParameter("cpass");				/// Confirm password
 		
 		if(pass.equals(cpass)) {
@@ -38,17 +38,17 @@ public class RegisterController extends HttpServlet {
 			int result = UserDao.setUser(u);
 			
 			if(result == 0) {
-				response.sendRedirect("login.jsp");																		/// Data was entered into database successfully
+				response.sendRedirect("login.jsp");										/// Data was entered into database successfully
 			}
 			else {
 				if(result == 1) {
-					request.setAttribute("errorMessage","User name already exists!");									/// User name exists in database
+					request.setAttribute("errorMessage","User name already exists!");					/// User name exists in database
 				}
 				if(result == 2) {
 					request.setAttribute("errorMessage","Data was not entered into database! Please try again.");		/// Error when entering data into the database
 				}
 				if(result == 3) {
-					request.setAttribute("errorMessage","An error occurred. Please try again.");						/// The setUser() method terminated prematurely
+					request.setAttribute("errorMessage","An error occurred. Please try again.");				/// The setUser() method terminated prematurely
 				}
 				request.getRequestDispatcher("register.jsp").forward(request,response);
 			}
@@ -56,7 +56,7 @@ public class RegisterController extends HttpServlet {
 		}
 		else {
 			request.setAttribute("errorMessage","Passwords do not match!");
-			request.getRequestDispatcher("register.jsp").forward(request,response);										/// Password and Confirm Password did not match
+			request.getRequestDispatcher("register.jsp").forward(request,response);							/// Password and Confirm Password did not match
 		}
 	}
 
